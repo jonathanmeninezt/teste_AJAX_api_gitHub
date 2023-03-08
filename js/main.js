f52236103e842dd492aeff839f21f51cf30a4b67
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-03-07 21:55:00 -0300

    criação- recuperando dados- DOM/ name;bio;followens;location;avatar

--- js/main.js
+++ js/main.js
@@ -6,7 +6,31 @@ const loadData = () => {
     xhr.onreadystatechange = () =>{
         if (xhr.readyState === 4){
             const res = JSON.parse(xhr.responseText)
-            console.log(res)
+            
+            const avatarUrl = res.avatar_url
+            const name = res.name
+            const bio = res.bio
+            const followers = res.followers
+            const location = res.location
+
+            const avatarEl = document.createElement("img")
+            avatarEl.setAttribute("width", "128")
+            avatarEl.setAttribute("src", avatarUrl)
+           
+            const nameEl = document.createElement("h1")
+            nameEl.innerText = name
+
+            const bioEl = document.createElement("h2")
+            bioEl.innerText = bio
+            
+            const followersEl = document.createElement("p")
+            followersEl.innerText = followers
+            
+            const locationEl = document.createElement("span")
+            locationEl.innerText = location
+            console.log(locationEl)
+
+
         }
     }
 }
